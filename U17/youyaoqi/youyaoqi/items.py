f4204022a1ab10278ad8c3446a47086cf31636d3
IEdison <hcy1418@163.com>
2018-11-26 17:49:01 +0800

    利用scrapy框架爬取漫画网站U17.com

---
+++ U17/youyaoqi/youyaoqi/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class YouyaoqiItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    comic_id = scrapy.Field()
+    name = scrapy.Field()
+    cover = scrapy.Field()
+    category = scrapy.Field()
